66625de419b94aed8c6bef6ab588a92c477acabc
IEUser <IEUser>
2017-04-27 11:56:59 +0200

    Mqtt implementat

--- routes/time.js
+++ routes/time.js
@@ -4,7 +4,28 @@ var funcs = require('./time-funcs.js');
 
 var getAverageTime = funcs.getAverageTime;
 
-module.exports = function(router) {
+module.exports = function(router, mqttClient) {
+
+  mqttClient.subscribe('etorn/store/+/advance');
+
+  mqttClient.on('message', function(topic, message) {
+    var match = /etorn\/store\/(\w{24})\/(\w+)/.exec(topic);
+    var id = match[1];
+    var chan = match[2];
+
+    if (!match)
+      return;
+
+    if (chan === 'advance') {
+      getAverageTime(id, function(err, result) {
+        if (err)
+          return;
+
+        mqttClient.publish('etorn/store/' + id + '/aproxTime', '' + result);
+
+      });
+    }
+  });
 
   router.route('/averageTime/:store_id')
     .get(function(req, res) {
